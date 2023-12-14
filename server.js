585deb3eaba65110f571c7493eeaf37d9a87dddb
Gisela <giselamata27@gmail.com>
2023-12-14 14:04:29 -0500

    Added server.js file, and imported express, fs, and path. Also added PORT

---
+++ server.js
@@ -0,0 +1,11 @@
+// Express import
+const express = require("express");
+// File system import
+const fs = require("fs");
+// Path import
+const path = require("path");
+// Healper method for generating unique ids
+const uniqid = require("uniqid");
+
+// Port
+const PORT = process.env.PORT || 3001;
